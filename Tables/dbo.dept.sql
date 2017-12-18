dcc2cf676a4282a28285a1918bbc37d3b1a03a5e
DevartVasiliyK <DevartVasiliyK@devart.com>
2017-12-18 12:39:20 +0200

    add table

--- Tables/dbo.dept.sql
+++ Tables/dbo.dept.sql
@@ -1,8 +1,7 @@
 ﻿CREATE TABLE [dbo].[dept] (
   [DEPTNO] [int] NOT NULL,
   [DNAME] [varchar](14) NULL,
-  [LOC] [varchar](15) NULL,
-  [c1] [varchar](50) NULL,
+  [LOC] [varchar](18) NULL,
   PRIMARY KEY CLUSTERED ([DEPTNO])
 )
 ON [PRIMARY]
