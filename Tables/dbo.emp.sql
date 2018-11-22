b1b797487b4a95d7a3fecd4bd8e0280c0f2f9ea1
DATASOFT\YaroslavS <yaroslavs@devart.com>
2018-11-22 17:30:39 +0200

    Initial commit

---
+++ Tables/dbo.emp.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[emp] (
+  [EMPNO] [int] NOT NULL,
+  [ENAME] [varchar](10) NULL,
+  [JOB] [varchar](9) NULL,
+  [MGR] [int] NULL,
+  [HIREDATE] [datetime] NULL,
+  [SAL] [float] NULL,
+  [COMM] [float] NULL,
+  [DEPTNO] [int] NULL,
+  PRIMARY KEY CLUSTERED ([EMPNO])
+)
+ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[emp]
+  ADD FOREIGN KEY ([DEPTNO]) REFERENCES [dbo].[dept] ([DEPTNO])
+GO
