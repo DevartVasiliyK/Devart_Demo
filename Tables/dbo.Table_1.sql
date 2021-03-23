e5857a1ef05da2cc311bd35ad2db457e53699a49
DevartVasiliyK <11316971+DevartVasiliyK@users.noreply.github.com>
2021-03-23 10:24:52 +0200

    Delete Tables directory

--- Tables/dbo.Table_1.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Table_1] (
-  [c1] [nchar](10) NULL,
-  [c2] [nchar](10) NULL
-)
-ON [PRIMARY]
-GO
--- Tables/dbo.child.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[child] (
-  [id] [int] NOT NULL,
-  [c1] [int] NULL,
-  [c2] [ntext] NULL,
-  CONSTRAINT [PK_child_id] PRIMARY KEY CLUSTERED ([id])
-)
-ON [PRIMARY]
-TEXTIMAGE_ON [PRIMARY]
-GO
